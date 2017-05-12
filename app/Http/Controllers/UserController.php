e483b95c5d6a55e145e0a99980cd542b84448166
Nico Plop <nyckoo@live.fr>
2017-05-12 09:46:32 -0400

    CorsMiddleware

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,7 +21,7 @@ class UserController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth', ['only' => ['show', 'index', 'update', 'destroy', 'store']]);
+        $this->middleware('auth', ['only' => ['show', 'index', 'destroy', 'store']]);
     }
 
     public function show($id)
