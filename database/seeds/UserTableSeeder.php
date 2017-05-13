27f8c0baab797e0ded10fe9dcbd8c25ea95bd08e
Nico Plop <nyckoo@live.fr>
2017-05-13 12:29:57 -0400

    new user

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -20,6 +20,22 @@ class UserTableSeeder extends Seeder
             'api_token' => str_random(32),
         ]);
         DB::table('user')->insert([
+            'firstname' => 'Seb Admin',
+            'lastname' => 'Amdin',
+            'email' => 'seb@nicolasmercier.io',
+            'password' => '85adbd0ee8c89bdb7cc320e7a9f78833f741492d',
+            'role' => 'admin',
+            'api_token' => str_random(32),
+        ]);
+        DB::table('user')->insert([
+            'firstname' => 'Camouille',
+            'lastname' => 'Admin',
+            'email' => 'camille@nicolasmercier.io',
+            'password' => 'b920592808acec58c9833234ce6265ad888f29a6',
+            'role' => 'admin',
+            'api_token' => str_random(32),
+        ]);
+        DB::table('user')->insert([
             'firstname' => 'Nicolas',
             'lastname' => 'Mercier',
             'email' => 'nyckoo@live.fr',
